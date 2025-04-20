e2b3dcb47421bdf204d49d4290d0a2511e249be4
Muhammad Tausif <tausifasia@gmail.com>
2025-04-20 07:50:32 +0500

    Simple first poster

--- linkedin/posters/poster_simple.py
+++ linkedin/posters/poster_simple.py
@@ -13,3 +13,10 @@ class LinkedInPoster(Scene):
         # Optional: add your logo or QR code
         # logo = ImageMobject("my_logo.png").scale(0.5).to_corner(UR)
         # self.add(logo)
+
+
+## manim poster.py LinkedInPoster -p -s -r 1200,628
+## manim poster.py LinkedInPoster -p -s -r 628,1200
+## manim poster.py LinkedInPoster -p -r 628,1200
+
+## ffmpeg -i complete_path\manim-videos\linkedin\posters\media\videos\poster_simple\1080p60\LinkedInPoster.mp4  -vf "fps=15,scale=1200:628:flags=lanczos"     complete_path\manim-videos\linkedin\posters\media\videos\poster_simple\LinkedInPoster.gif
